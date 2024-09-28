6628d748cea00eadd2a0c9351ee4b1d02a0672b5
yunma-code <ma.yun2@northeastern.edu>
2024-09-28 16:29:09 -0400

    lab2 in-progress

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,4 +1,18 @@
 import "./index.css";
+import ForegroundColors from "./ForegroundColors";
+import BackgroundColors from "./BackgroundColors";
+import Borders from "./Borders";
+import Padding from "./Padding";
+import Margins from "./Margins";
+import Corners from "./Corners";
+import Dimensions from "./Dimensions";
+import Positions from "./Positions";
+import Zindex from "./Zindex";
+import Float from "./Float";
+import GridLayout from "./GridLayout";
+import Flex from "./Flex";
+import ReactIcons from "./ReactIcons";
+
 export default function Lab2() {
 	return(
 		<div id="wd-lab2">
@@ -10,8 +24,59 @@ export default function Lab2() {
 				it is considered bad practice and you should avoid
 				using the style attribute
 			</p>
-			<h2>Colors</h2>
-				<h3 className="fg-color-blue">
+
+			<div id="wd-css-id-selectors">
+				<h3> ID selectors</h3>
+				<p id="wd-id-selector-1">
+					Instead of changing the look and feel of all the
+					elements of the same name, e.g., P, we can refer
+					to a specific element by its ID
+				</p>
+				<p id="wd-id-selector-2">
+					Here's another paragraph using a different ID and
+					a different look and feel
+				</p>
+			</div>
+
+			<div id="wd-css-class-selectors">
+				<h3>Class selectors</h3>
+				<p className="wd-class-selector">
+			Instead of using IDs to refer to elements, you can use an element's CLASS attribute
+				</p>
+				<h4 className="wd-class-selector">
+			This heading has same style as paragraph above
+				</h4>
+			</div>
+			<div id="wd-css-document-structure">
+				<div className="wd-selector-1">
+					<h3>Document structure selectors</h3>
+					<div className="wd-selector-2">
+						Selectors can be combined to refer elements in particular
+						places in the document
+						<p className="wd-selector-3">
+							This paragraph's red background is referenced as
+							<br />
+							.selector-2 .selector3<br />
+							meaning the descendant of some ancestor.<br />
+							<span className="wd-selector-4">
+								Whereas this span is a direct child of its parent
+							</span><br />
+								You can combine these relationships to create specific 
+								styles depending on the document structure
+						</p>
+					</div>
+				</div>
+			</div>
+			<ForegroundColors /> <BackgroundColors />
+			<Borders /><Padding /> <Margins /> <Corners />
+			<Dimensions /> <Positions /> <Zindex /> <Float /> 
+			<GridLayout /> <Flex /> <ReactIcons />
+		
+
+			</div>	
+	);
+}
+				{/* <h3 className="fg-color-blue">
 						Foreground color</h3>
 				<p className="fg-color-red">
 						The text in this paragraph is red but
@@ -30,13 +95,20 @@ export default function Lab2() {
 					foreground white</span>
 				</p>
 
-			<h1>Block vs inline elements</h1>
+			<h1 style={{backgroundColor: "yellow"}}>Block vs inline elements</h1>
 			<p>Headings and paragraphs are block elements. Width is parent's width</p>
 
 			Normal text renders inline <br /><br />
 			<span>Span elements</span>
 			<span>render inline</span> with the rest of the content
 
-		</div>	
-	);
-}
+			<h1 style={{
+				backgroundColor: "yellow"}}>Block</h1>
+			<p style={{backgroundColor: "blue", color:"white"}}>
+				Headings ... width</p>
+				Normal text renders inline <br/><br/>
+				<span style={{
+					backgroundColor: "red", color:"white"}}>Span elements</span>
+				<span style={{backgroundColor: "red", color: "white"}}>render inline</span>
+				with the rest of the content */}
+			
